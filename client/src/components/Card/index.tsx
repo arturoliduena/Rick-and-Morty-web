fa23522a5f0a2d27a2db5005ce794cbbfdd6e455
Arturo Lidueña <arturo.liduena@gmail.com>
2020-12-29 15:28:32 +0100

    adding card character styles

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,46 +1,30 @@
 import React from 'react';
 import styles from './Card.module.css';
-import Button from '../Button';
-import { ICard } from '../../types';
 import notFound from '../../assets/notFound.jpeg';
-import { useDispatch } from 'react-redux';
-import { openModalLogin } from '../../store/actions/modal';
 
 interface Props {
-  item: ICard | any
+  image: string,
+  infoElement: JSX.Element,
+  extraInfoElement: JSX.Element,
+  title: string,
 }
-const Card = ({ item }: Props) => {
-  const dispatch = useDispatch();
-  const { id, title, description, image } = item;
-
+const Card = ({ image, infoElement, extraInfoElement, title }: Props) => {
   const defaultImg = (event: React.ChangeEvent<HTMLImageElement>) => {
     event.target.src = notFound
   };
-
-  const editItem = () => {
-    dispatch(openModalLogin())
-  };
-
-  const deleteItem = () => {
-    // dispatch(deleteCard(id))
-  }
-
   return (
     <div className={styles.container}>
       <div className={styles.page_content} >
       <img className={styles.img} src={image} alt='' onError={defaultImg} />
         <div className={styles.card} >
           <div className={styles.content} >
-            <h2 className={styles.title} >{'title'}</h2>
-            <div className={styles.button_group}>
-              <Button text='Edit' onClick={editItem} />
-              <Button text='Delete' onClick={deleteItem} />
-            </div>
+          <h2 className={styles.title} >{title}</h2>
+            { extraInfoElement }
           </div>
         </div>
       </div>
       <div className={styles.description}>
-        <p className={styles.text} >{'description'}</p>
+        { infoElement }
       </div>
     </div>
   );
--- client/src/components/Characters/index.tsx
+++ client/src/components/Characters/index.tsx
@@ -5,50 +5,67 @@ import { RootState } from '../../store';
 import Card from '../Card';
 import { ICharacter } from '../../types';
 import styles from './Characters.module.css';
-
-interface Props {
-  characters: string[],
-}
+import Button from '../Button';
 
 interface PropsCharacter {
   character: ICharacter,
 }
 
-const Character = ({ character }: PropsCharacter) => {
+const Status = ({ status }: { status: ICharacter['status']}) => {
+  const statusColor = status == 'Alive' ? styles.statusColorGreen : styles.statusColorRed;
   return (
-    character ? <Card item={character}/> :
-      null
+    <div className={[styles.status, statusColor].join(' ')}/>
   )
+}
+const buildInfo = ({ name, status, species, origin, episode }: ICharacter) => {
+  return (
+    <div className={styles.content}>
+      <div className={styles.title}>{name}</div>
+      <div className={styles.subtitle}><Status status={status}/>{status} - {species}</div>
+      <div className={styles.label}>Origin</div>
+      <div className={styles.text}>{origin.name}</div>
+      <div className={styles.label}>Episodes</div>
+      <div className={styles.text}>{episode.length}</div>
+    </div>
+  )
+}
+
+const buildExtaInfo = ({ id }: ICharacter) => {
+  return (
+    <div className={styles.button_group}>
+      <Button text='Add Fav' onClick={() => console.log('click')} />
+      <Button text='Remove Fav' onClick={() => console.log('click')} />
+    </div>
+  )
+}
+
+const Character = ({ character }: PropsCharacter) => {
+  const infoElement = buildInfo(character)
+  const extraInfoElement = buildExtaInfo(character)
+  return <Card image={character.image} infoElement={infoElement} extraInfoElement={extraInfoElement} title='Fav' />
 };
 
 const Characters = () => {
-  const [expanded, setExpanded] = useState(true);
-
-  const handlePress = () => setExpanded(!expanded);
-
   const dispatch = useDispatch()
   const characters = useSelector((state: RootState) => state.character.characters);
   const info = useSelector((state: RootState) => state.character.info);
   const [page, setPage] = useState(1);
-  
-  // const navigator = (id: number) => () => navigation.navigate('Character', { itemId: id});
 
   useEffect(() => {
     dispatch(getCharacters(page));
   }, [page])
 
   const onPageChange = (nextPage: number) => {
-    if(nextPage && nextPage <= info.pages){
+    if (nextPage && nextPage <= info.pages) {
       setPage(nextPage);
     }
   }
 
-
   return (
     <div className={styles.body}>
-          {
-            characters.map((character: ICharacter) => <Character key={character.id} character={character}/>)
-          }
+      {
+        characters.map((character: ICharacter) => <Character key={character.id} character={character} />)
+      }
     </div>
   );
 };
